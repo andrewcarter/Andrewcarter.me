8c4e842777a17105c3c0454112d05bd48821b94e
Chris Marsh <chrismarsh.c2@gmail.com>
2011-03-30 14:24:48 -0600

    Significant modifications to the template. Essentially removed all the multimedia stuff for now.

--- conf/config.php
+++ conf/config.php
@@ -1,26 +1,34 @@
 <?php
 
 /*** VISUAL STYLE ***/
-$visual_style['background_image'] = ''; // Enter any full URL to a photo to use it as your background photo
+//$visual_style['background_image'] = 'https://fbcdn-sphotos-a.akamaihd.net/hphotos-ak-ash1/29224_10150164188020246_660645245_12258936_7573673_n.jpg'; // Enter any full URL to a photo to use it as your background photo
 $visual_style['navigation_shadows'] = false; // Add shadows to your name and the navigation elements to make them easier to read on complex background images
-$visual_style['navigation_color'] = '#FFFFFF'; // Set this to any HEX color value to change the color of your name and navigation
+$visual_style['navigation_color'] = '#000000';
+//$visual_style['navigation_color'] = '#6C0'; // Set this to any HEX color value to change the color of your name and navigation
 
 /*** ABOUT ME ***/
-$general['first_name'] = 'First'; // Just your first name
-$general['last_name'] = 'Last'; // Just your last name
-$general['about_me'] = ""; // This is where you can type information about yourself.  If you use any quotation marks, be sure to put a \ before them.
+$general['first_name'] = 'Chris'; // Just your first name
+$general['last_name'] = 'Marsh'; // Just your last name
+$general['about_me'] = 
+"I am currently a masters student in Physical Geography at the <a href=\"http://www.usask.ca/\">University of Saskatchewan</a> in Saskatchewan, Canada. I have an interest in Earth systems  modelling and related fieldwork.
 
-/*** PHOTOS ***/
-$general['about_photos'] = 'These are my awesome Flickr photos!'; // This is description that shows up in your photos section
-$accounts['flickr']['username'] = ''; // The username in your URL, if your photo URL is http://flickr.com/photos/lifehacker your username would be lifehacker
-$accounts['flickr']['apikey'] = ''; // Looks like dfc510ae6d2c45d5fdf9259415pe7c92, and you can sign up for one (if you don't have one already) at http://www.flickr.com/services/apps/create/apply
 
-/*** VIDEOS ***/
-$general['about_videos'] = 'These are my awesome Vimeo and YouTube videos!'; // This is a description of
-$accounts['vimeo']['username'] = ''; // Entering your Vimeo username will display your public Vimeo videos on your page
-$accounts['youtube']['username'] = ''; // Entering your YouTube username will display your public YouTube videos on your page
 
-/*** TWITTER ***/
-$accounts['twitter']['username'] = ''; // Entering your Twitter username will display your latest tweets on your page
 
+
+  Honours BSc. with:</p>
+
+  <p>Physical Geography - Major<br />
+
+    Computer Science - Minor<br />
+
+    Mathematics - Minor</p>
+
+"
+;
+$general['CV']='test cv';
+
+/*** TWITTER 
+$accounts['twitter']['username'] = 'sigmac2'; // Entering your Twitter username will display your latest tweets on your page
+***/
 ?>
--- js/switches.js
+++ js/switches.js
@@ -1,26 +1,41 @@
 function switchto(elem)
 {
-	document.getElementById('about').style.display = document.getElementById('photos').style.display = document.getElementById('videos').style.display = document.getElementById('twitter').style.display = 'none';
-	document.getElementById(elem).style.display = 'inline';
-	if (elem == 'about')
+	if(collapsed == false && section == elem)
 	{
-		triangle_margin = '10px 54px 0 0';
-	}
-	else if (elem == 'photos')
-	{
-		triangle_margin = '10px 164px 0 0';
-	}
-	else if (elem == 'videos')
-	{
-		triangle_margin = '10px 276px 0 0';
-	}
-	else if (elem == 'twitter')
-	{
-		triangle_margin = '10px 389px 0 0';
+		collapsed = true;
+		section = "";
+		hideall();	
 	}
 	else
 	{
-		triangle_margin = '10px 54px 0 0';
+		hideall();
+		document.getElementById(elem).style.display = 'inline';
+		if (elem == 'about')
+		{
+			triangle_margin = '10px 54px 0 0';
+		}
+		else if (elem == 'CV')
+		{
+			triangle_margin = '10px 170px 0 0';
+		}
+		else
+		{
+			triangle_margin = '10px 54px 0 0';
+		}
+		document.getElementById('triangle').style.display = 'inline';
+		document.getElementById('triangle').style.margin = triangle_margin;
+		document.getElementById('footer').style.display = 'inline';
+		
+		section = elem;
+		collapsed = false;
 	}
-	document.getElementById('triangle').style.margin = triangle_margin;
+}
+
+function hideall()
+{
+		document.getElementById('about').style.display = 'none';
+		document.getElementById('CV').style.display = 'none';
+		document.getElementById('triangle').style.display = 'none';
+		document.getElementById('footer').style.display = 'none';
+
 }
